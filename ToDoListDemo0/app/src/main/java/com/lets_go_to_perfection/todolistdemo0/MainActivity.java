d62ecce1a6a3646ccbad0680dd70f0391bd9fe8b
Hossam Hassan <hossamhassancs15@gmail.com>
2016-12-04 03:14:08 +0200

    Use sharedPreferences in ToDoListDemo0

--- ToDoListDemo0/app/src/main/java/com/lets_go_to_perfection/todolistdemo0/MainActivity.java
+++ ToDoListDemo0/app/src/main/java/com/lets_go_to_perfection/todolistdemo0/MainActivity.java
@@ -1,7 +1,9 @@
 package com.lets_go_to_perfection.todolistdemo0;
 
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -12,23 +14,35 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-
+    SharedPreferences prefs;
+    private final static String DATA_KEY = "data_key";
     ListView listTasks;
     ArrayAdapter adapter;
-    List<String> data;
+    ArrayList<String> data;
     private final static String TAG = "TodoListDemo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+
+        if (savedInstanceState == null) {
+            data = getData();
+            if (data == null || data.isEmpty()) {
+                data = new ArrayList<>();
+            }
+
+        } else {
+            data = savedInstanceState.getStringArrayList(DATA_KEY);
+        }
+
         listTasks = (ListView) findViewById(R.id.listNotes);
-        data = new ArrayList<>();
-        data.add("Note 1");
-        data.add("Note 2");
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, data);
         listTasks.setAdapter(adapter);
 
@@ -71,4 +85,37 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putStringArrayList(DATA_KEY, data);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        saveData();
+    }
+
+    private void saveData() {
+        SharedPreferences.Editor editor = prefs.edit();
+        Set<String> set = new HashSet<>();
+        set.addAll(data);
+        editor.putStringSet(DATA_KEY, set);
+        editor.apply();
+    }
+
+    private ArrayList<String> getData() {
+        Set<String> set = new HashSet<>();
+        data = new ArrayList<>();
+        set = prefs.getStringSet(DATA_KEY, null);
+        if (set != null) {
+            for (String s : set) {
+                data.add(s);
+            }
+        }
+        return data;
+    }
+
 }
